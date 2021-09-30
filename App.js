5b74c95b311d81454611391af10d20cea3eda478
Shalini-CEG <ahana298@gmail.com>
2021-09-30 14:56:55 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,154 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import { Calendar } from "react-native-calendars";
+import WebViews from "./WebViews.js";
 
-export default function App() {
+const App = () => {
+  const [reminder, setReminder] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [selectedDate, setSelectedDate] = useState("");
+  const [note, setNote] = useState("");
+  const [uri, setUri] = useState(false);
+
+  const handlePress = (val) => {
+    setSelectedDate(val.dateString);
+    setShowForm(true);
+  };
+
+  const handleSave = () => {
+    if (note.length > 0) {
+      setReminder((prev) => [
+        ...prev,
+        {
+          date: selectedDate,
+          note: note,
+        },
+      ]);
+    }
+
+    setSelectedDate("");
+    setNote("");
+    setShowForm(false);
+  };
+
+  const handleCancel = () => {
+    setSelectedDate("");
+    setNote("");
+    setShowForm(false);
+  };
+
+  function clicked() {
+    console.log(uri + " uri");
+    setUri(!uri);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View>
+        <TouchableOpacity style={{ margin: 10 }} onPress={clicked}>
+          <Text style={{ color: "blue" }}>Open website</Text>
+        </TouchableOpacity>
+      </View>
+      {uri ? (
+        <WebViews />
+      ) : (
+        <View>
+          <View style={{ paddingVertical: 10 }}>
+            {!showForm ? (
+              <ScrollView>
+                <Text style={styles.reminderHeading}>List of Reminders</Text>
+                {reminder.length > 0 ? (
+                  reminder.map((item, index) => (
+                    <Text key={index} style={styles.reminders}>
+                      {item.date} {"  "} {item.note}
+                    </Text>
+                  ))
+                ) : (
+                  <Text style={styles.reminders}>No reminders added</Text>
+                )}
+              </ScrollView>
+            ) : (
+              <View>
+                <Text style={styles.reminderHeading}>Add A Reminder</Text>
+                <Text style={{ padding: 10, fontSize: 20 }}>
+                  {selectedDate}
+                </Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={(text) => setNote(text)}
+                  value={note}
+                  placeholder="Remind me to..."
+                />
+                <View style={styles.btnContainer}>
+                  <Pressable style={styles.btn} onPress={handleSave}>
+                    <Text style={{ color: "white" }}>SAVE</Text>
+                  </Pressable>
+
+                  <Pressable style={styles.btn} onPress={handleCancel}>
+                    <Text style={{ color: "white" }}>CANCEL</Text>
+                  </Pressable>
+                </View>
+              </View>
+            )}
+          </View>
+          <View>
+            <Calendar
+              hideExtraDays
+              enableSwipeMonths
+              onDayPress={(val) => handlePress(val)}
+            />
+          </View>
+        </View>
+      )}
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  reminderHeading: {
+    borderBottomColor: "#00008B",
+    borderBottomWidth: 5,
+    paddingBottom: 5,
+    fontSize: 25,
+  },
+  btn: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+    marginHorizontal: 10,
+  },
+  reminders: {
+    fontSize: 20,
+    paddingVertical: 15,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  input: {
+    fontSize: 20,
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  btnContainer: {
+    paddingTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    //justifyContent: "space-around",
   },
 });
+
+export default App;
---
+++ WebViews.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { WebView } from "react-native-webview";
+
+function WebViews() {
+  return <WebView source={{ uri: "https://yahoo.com" }} />;
+}
+
+export default WebViews;
