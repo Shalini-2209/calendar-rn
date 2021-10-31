4db45407affef4a8f44afc64365842de53400782
Shalini-CEG <ahana298@gmail.com>
2021-10-31 17:31:58 +0530

    Links updated 📎

--- WebViews.js
+++ WebViews.js
@@ -2,7 +2,7 @@ import React from "react";
 import { WebView } from "react-native-webview";
 
 function WebViews() {
-  return <WebView source={{ uri: "https://yahoo.com" }} />;
+  return <WebView source={{ uri: "https://mail.google.com/mail/u/0/#inbox" }} />;
 }
 
 export default WebViews;
